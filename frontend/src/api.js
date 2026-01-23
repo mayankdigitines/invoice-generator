4f411e97d5f1fcb2626ea4ba9e7a2cb4d9426e0e
mayank <mayankmayank1225@gmail.com>
2026-01-24 04:08:32 +0530

    update API base URL to production endpoint

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://invoice-generator-backend-e3qj8zla4.vercel.app/api',
 });
 
 export default api;
